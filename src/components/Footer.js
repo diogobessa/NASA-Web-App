46a0cf9cecac99236848441c6d5fdf5484d69221
Diogo Bessa <diogobessawd@gmail.com>
2020-01-03 23:53:42 +0000

    footer basic styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Footer = () => {
-    return <footer><p>footer comp</p></footer>
+    return (
+    <footer>
+        <p>NASA @ Astronomy Picture of the Day</p>
+        <style jsx global>{`
+            footer{
+                margin:10rem 5rem 0;
+                padding:2rem;
+                box-sizing: border-box;
+                border-top:1px solid grey;
+            }
+            footer > p {
+                text-align: center;
+                font-size: 1rem;
+                padding:2rem;
+            }
+        `}</style>    
+    </footer>
+    )
 }
 
 export default Footer;
